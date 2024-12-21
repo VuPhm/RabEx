12ae48324507f541f17bf74bf5dd4dcfa0d7c647
NgoVan <127534396+NgoCuVan@users.noreply.github.com>
2024-12-21 14:48:10 +0700

    add backend order-history function

---
+++ src/main/java/com/rabex/express/controllers/user/OrderHistoryController.java
@@ -0,0 +1,18 @@
+package com.rabex.express.controllers.user;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/lich-su-buu-pham")
+public class OrderHistoryController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/user/order-history.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/java/com/rabex/express/services/impl/OrderHistoryServiceImpl.java
@@ -0,0 +1,18 @@
+package com.rabex.express.services.impl;
+
+import com.rabex.express.model.Order;
+import com.rabex.express.services.OrderHistoryService;
+
+import java.util.List;
+
+public class OrderHistoryServiceImpl implements OrderHistoryService {
+    @Override
+    public List<Order> getAllOrder() {
+        return null;
+    }
+
+    @Override
+    public int countAll() {
+        return 0;
+    }
+}
