093e7c5dd9792277cc4849f87bbab7765584757b
NgoVan <127534396+NgoCuVan@users.noreply.github.com>
2024-12-27 20:06:36 +0700

    update customer service

--- src/main/java/com/rabex/express/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/rabex/express/services/impl/CustomerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.rabex.express.services.impl;
 
 import com.rabex.express.core.dao.RID;
+import com.rabex.express.dao.AddressDao;
 import com.rabex.express.dao.CustomerDao;
 import com.rabex.express.model.Customer;
 import com.rabex.express.services.CustomerService;
@@ -11,6 +12,8 @@ import java.util.List;
 public class CustomerServiceImpl implements CustomerService {
     @Inject
     private CustomerDao customerDao;
+    @Inject
+    private AddressDao addressDao;
 
     @Override
     public List<Customer> findAll() {
@@ -25,5 +28,15 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public Customer findById(RID id) {
         return customerDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public boolean delete(RID id) {
+        return customerDao.deleteById(id);
+    }
+
+    @Override
+    public boolean deleteAddress(RID addressId, RID customerId) {
+        return false;
     }
 }
