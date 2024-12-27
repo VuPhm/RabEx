745a660e6836167304be9c0b18928c0d21824a5a
NgoVan <127534396+NgoCuVan@users.noreply.github.com>
2024-12-27 20:07:48 +0700

    update OrderHistoryServiceImpl

--- src/main/java/com/rabex/express/services/impl/OrderHistoryServiceImpl.java
+++ src/main/java/com/rabex/express/services/impl/OrderHistoryServiceImpl.java
@@ -1,11 +1,13 @@
 package com.rabex.express.services.impl;
 
+import com.rabex.express.dao.OrderHistoryDao;
 import com.rabex.express.model.Order;
 import com.rabex.express.services.OrderHistoryService;
 
 import java.util.List;
 
 public class OrderHistoryServiceImpl implements OrderHistoryService {
+    OrderHistoryDao orderHistory;
     @Override
     public List<Order> getAllOrder() {
         return null;
