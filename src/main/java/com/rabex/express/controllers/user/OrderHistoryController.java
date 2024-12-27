8b28f40d546117f9c9e07ba0ab5d6cbe7b797fc7
NgoVan <127534396+NgoCuVan@users.noreply.github.com>
2024-12-27 20:07:39 +0700

    update OrderHistoryController

--- src/main/java/com/rabex/express/controllers/user/OrderHistoryController.java
+++ src/main/java/com/rabex/express/controllers/user/OrderHistoryController.java
@@ -13,6 +13,7 @@ public class OrderHistoryController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         req.getRequestDispatcher("/WEB-INF/views/user/order-history.jsp").forward(req, resp);
     }
 }
