1411389600789c6a5bf72bd78115f5e05d8c692a
NgoVan <127534396+NgoCuVan@users.noreply.github.com>
2025-01-14 23:48:06 +0700

    add database sql

--- data/sample/customers.sql
+++ data/sample/customers.sql
@@ -1,10 +1,11 @@
-INSERT INTO rabex.customers (id, default_address_id, phone_number, full_name, email, company_name, industry, channel) VALUES ('01HZY0M93WZXABCDEF12345711', '01HZY0M93WZXABCDEF12345679', '0901234567', 'Nguyễn Văn A', 'nguyen.vana@example.com', 'Công ty A', 'Retail', 'Online');
-INSERT INTO rabex.customers (id, default_address_id, phone_number, full_name, email, company_name, industry, channel) VALUES ('01HZY0M93WZXABCDEF12345712', '01HZY0M93WZXABCDEF12345692', '0987654321', 'Trần Thị B', 'tran.thib@example.com', 'Công ty B', 'Technology', 'Offline');
-INSERT INTO rabex.customers (id, default_address_id, phone_number, full_name, email, company_name, industry, channel) VALUES ('01HZY0M93WZXABCDEF12345713', '01HZY0M93WZXABCDEF12345700', '0912345678', 'Lê Minh C', 'le.minhc@example.com', 'Công ty C', 'Manufacturing', 'Online');
-INSERT INTO rabex.customers (id, default_address_id, phone_number, full_name, email, company_name, industry, channel) VALUES ('01HZY0M93WZXABCDEF12345714', '01HZY0M93WZXABCDEF12345707', '0934567890', 'Phạm Văn D', 'pham.vand@example.com', 'Công ty D', 'Finance', 'Offline');
-INSERT INTO rabex.customers (id, default_address_id, phone_number, full_name, email, company_name, industry, channel) VALUES ('01HZY0M93WZXABCDEF12345715', '01HZY0M93WZXABCDEF12345735', '0971234567', 'Đặng Thị E', 'dang.thie@example.com', 'Công ty E', 'Healthcare', 'Online');
-INSERT INTO rabex.customers (id, default_address_id, phone_number, full_name, email, company_name, industry, channel) VALUES ('01HZY0M93WZXABCDEF12345716', '', '0907654321', 'Hoàng Minh F', 'hoang.minhf@example.com', 'Công ty F', 'Education', 'Offline');
-INSERT INTO rabex.customers (id, default_address_id, phone_number, full_name, email, company_name, industry, channel) VALUES ('01HZY0M93WZXABCDEF12345717', '', '0986543210', 'Bùi Thị G', 'bui.thig@example.com', 'Công ty G', 'Retail', 'Online');
-INSERT INTO rabex.customers (id, default_address_id, phone_number, full_name, email, company_name, industry, channel) VALUES ('01HZY0M93WZXABCDEF12345718', '', '0918765432', 'Ngô Minh H', 'ngo.minhh@example.com', 'Công ty H', 'Technology', 'Offline');
-INSERT INTO rabex.customers (id, default_address_id, phone_number, full_name, email, company_name, industry, channel) VALUES ('01HZY0M93WZXABCDEF12345719', '', '0932345678', 'Cao Thị I', 'cao.thii@example.com', 'Công ty I', 'Finance', 'Online');
-INSERT INTO rabex.customers (id, default_address_id, phone_number, full_name, email, company_name, industry, channel) VALUES ('01HZY0M93WZXABCDEF12345720', '', '0976543210', 'Phan Minh J', 'phan.minhj@example.com', 'Công ty J', 'Healthcare', 'Offline');
+insert into rabex.customers (id, default_address_id, phone_number, full_name, email, company_name, industry, channel)
+values  ('01HZY0M93WZXABCDEF12345711', '01HZY0M93WZXABCDEF12345679', '0987654321', 'Nguyễn Thị Giàu', 'ngu6346@gmail.com', 'Công ty FPT', 'Công nghệ', 'Online'),
+        ('01HZY0M93WZXABCDEF12345712', '01HZY0M93WZXABCDEF12345692', '0976543210', 'Trần Thị Bơi', 'tranthib71385@gmail.com', 'Công ty FPT', 'Tài chính', 'Offline'),
+        ('01HZY0M93WZXABCDEF12345713', '01HZY0M93WZXABCDEF12345700', '0965432109', 'Lê Văn Cừ', 'levanc149@gmail.com', 'Công ty FPT', 'Y tế', 'Online'),
+        ('01HZY0M93WZXABCDEF12345714', '01HZY0M93WZXABCDEF12345707', '0954321098', 'Phạm Thị Thuỳ Duyên', 'phamthid80394@gmail.com', 'Công ty FPT', 'Giáo dục', 'Offline'),
+        ('01HZY0M93WZXABCDEF12345715', '01HZY0M93WZXABCDEF12345735', '0943210987', 'Hoàng Văn Công Tôn', 'hoangvane10001@gmail.com', 'Công ty giải trí Garena', 'Thương mại', 'Online'),
+        ('01HZY0M93WZXABCDEF12345716', null, '0932109876', 'Võ Việt Pha', 'vothif20045@gmail.com', 'Công ty giải trí Garena', 'Dịch vụ', 'Offline'),
+        ('01HZY0M93WZXABCDEF12345717', null, '0921098765', 'Đặng Văn Bi', 'dangvang2025@gmail.com', 'Công ty Shopee', 'Vận tải', 'Online'),
+        ('01HZY0M93WZXABCDEF12345718', null, '0910987654', 'Ngô Thị Ngọc Xuyến', 'ngothih2@gmail.com', 'Công ty Nông Lâm', 'Nông nghiệp', 'Offline'),
+        ('01HZY0M93WZXABCDEF12345719', null, '0909876543', 'Bùi Văn Lộc', 'buivani@gmail.com', 'Công ty TNHH Robber', 'Xây dựng', 'Online'),
+        ('01HZY0M93WZXABCDEF12345720', null, '0898765432', 'Đinh Công Tuấn', 'dinhthij135135@gmail.com', 'Công ty F-Soft', 'Du lịch', 'Offline');
