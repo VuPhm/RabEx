6ab11544fd997a5015d4452a262bb2609ecfed6f
NgoVan <127534396+NgoCuVan@users.noreply.github.com>
2025-01-14 13:02:59 +0700

    update views order history

--- src/main/java/com/rabex/express/services/OrderHistoryService.java
+++ src/main/java/com/rabex/express/services/OrderHistoryService.java
@@ -1,9 +1,11 @@
 package com.rabex.express.services;
 
+import com.rabex.express.core.dao.RID;
 import com.rabex.express.model.Order;
 
 import java.util.List;
 
 public interface OrderHistoryService {
     public List<Order> getOrderHistory();
+    public Order getOrderDetails(RID oid);
 }
--- src/main/java/com/rabex/express/services/impl/OrderHistoryServiceImpl.java
+++ src/main/java/com/rabex/express/services/impl/OrderHistoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.rabex.express.services.impl;
 
-import com.rabex.express.dao.OrderHistoryDao;
+import com.rabex.express.core.dao.RID;
+import com.rabex.express.dao.OrderDao;
 import com.rabex.express.model.Order;
 import com.rabex.express.services.OrderHistoryService;
 import jakarta.inject.Inject;
@@ -9,11 +10,16 @@ import java.util.List;
 
 public class OrderHistoryServiceImpl implements OrderHistoryService {
     @Inject
-    OrderHistoryDao orderHistoryDao;
+    OrderDao orderHistoryDao;
 
 
     @Override
     public List<Order> getOrderHistory() {
         return orderHistoryDao.findAll();
+    }
+
+    @Override
+    public Order getOrderDetails(RID oid) {
+        return orderHistoryDao.findById(oid).orElse(null);
     }
 }
