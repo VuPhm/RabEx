21ad38ab89c37e9958a3255836d2b1c9e3d03776
Pham Le Truong Vu <20130469@st.hcmuaf.edu.vn>
2025-01-15 19:22:21 +0700

    update about us page

---
+++ src/main/java/com/rabex/express/controllers/guest/About.java
@@ -0,0 +1,17 @@
+package com.rabex.express.controllers.guest;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/ve-chung-toi")
+public class About extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/guest/about.jsp").forward(req, resp);
+    }
+}
