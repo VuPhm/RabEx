bb643c7b7ab68b164e18a4ef742976915d739b49
Pham Le Truong Vu <20130469@st.hcmuaf.edu.vn>
2025-01-15 20:41:34 +0700

    update some pages vieư

---
+++ src/main/java/com/rabex/express/controllers/Login.java
@@ -0,0 +1,23 @@
+package com.rabex.express.controllers;
+
+import com.rabex.express.services.ShippingServService;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/dang-nhap")
+public class Login extends HttpServlet {
+//    @Inject
+//    private ShippingServService shippingServService;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        req.setAttribute("services", shippingServService.findAll());
+        req.getRequestDispatcher("/WEB-INF/views/guest/login.jsp").forward(req, resp);
+    }
+
+}
